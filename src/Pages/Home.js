7faed66430ed4f43bab03c95341ef777f72a054a
kdipto1 <kdipto@outlook.com>
2022-06-26 00:33:47 +0600

    index.js error fixed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div>gojh</div>;
 };
 
 export default Home;
