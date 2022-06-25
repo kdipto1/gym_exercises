19c48413fcbbf5499c2a966c916076e0034ddbf7
kdipto1 <kdipto@outlook.com>
2022-06-25 23:54:18 +0600

    router, material, loader spinner package , and home, exercise route added in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import ExerciseDetail from "./Pages/ExerciseDetail";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 };
 
